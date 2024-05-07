f211b1d004f10b0a9c60a83e4931e5799b20a01a
m3id <m3id.eprac@gmail.com>
2024-05-08 04:43:17 +0900

    feat(v2): exchange_code return Result

--- src/v2.rs
+++ src/v2.rs
@@ -1,14 +1,10 @@
-use std::{error, time::Duration};
-
-use anyhow::Context;
 use oauth2::{
-    basic::{BasicClient, BasicTokenType},
-    reqwest::async_http_client,
-    AuthUrl, AuthorizationCode, Client, ClientId, ClientSecret, CsrfToken, EmptyExtraTokenFields,
+    basic::{BasicClient, BasicErrorResponseType, BasicTokenType},
+    reqwest::{async_http_client, http_client},
+    AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, EmptyExtraTokenFields,
     PkceCodeChallenge, PkceCodeVerifier, RedirectUrl, RequestTokenError, Scope,
-    StandardTokenResponse, TokenUrl,
+    StandardErrorResponse, StandardTokenResponse, TokenUrl,
 };
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
 
 pub struct TwitchOauth(BasicClient);
 
@@ -45,39 +41,47 @@ impl TwitchOauth {
             .url()
     }
 
-    pub async fn exchange_token(
+    pub async fn exchange_token_async(
         &self,
         auth_code: AuthorizationCode,
         pkce_verifier: PkceCodeVerifier,
-    ) -> oauth2::StandardTokenResponse<oauth2::EmptyExtraTokenFields, oauth2::basic::BasicTokenType>
-    {
+        // ) -> oauth2::StandardTokenResponse<oauth2::EmptyExtraTokenFields, oauth2::basic::BasicTokenType>
+    ) -> Result<
+        StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>,
+        RequestTokenError<
+            oauth2::reqwest::Error<reqwest::Error>,
+            StandardErrorResponse<BasicErrorResponseType>,
+        >,
+    > {
         self.0
             .exchange_code(auth_code)
             .set_pkce_verifier(pkce_verifier)
             .request_async(async_http_client)
             .await
-            .unwrap()
+    }
+
+    pub async fn exchange_token(
+        &self,
+        auth_code: AuthorizationCode,
+        pkce_verifier: PkceCodeVerifier,
+    ) -> Result<
+        StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>,
+        RequestTokenError<
+            oauth2::reqwest::Error<reqwest::Error>,
+            StandardErrorResponse<BasicErrorResponseType>,
+        >,
+    > {
+        self.0
+            .exchange_code(auth_code)
+            .set_pkce_verifier(pkce_verifier)
+            .request(http_client)
     }
 }
 
-#[derive(Debug, thiserror::Error)]
-pub enum OauthError {
-    #[error("timeout '{0}'")]
-    AcceptTimeout(#[from] tokio::time::error::Elapsed),
-    #[error("can't read stream '{0}'")]
-    ReadStream(String),
-}
-
-fn query_find_code(url: &url::Url) -> AuthorizationCode {
-    url.query_pairs()
-        .find(|(key, _)| key == "code")
-        .map(|(_, code)| AuthorizationCode::new(code.into_owned()))
-        .unwrap()
-}
-
-fn query_find_state(url: &url::Url) -> CsrfToken {
-    url.query_pairs()
-        .find(|(key, _)| key == "code")
-        .map(|(_, code)| CsrfToken::new(code.into_owned()))
-        .unwrap()
-}
+// #[derive(Debug, thiserror::Error)]
+// pub enum OauthError {
+//     #[error("timeout '{0}'")]
+//     AcceptTimeout(#[from] tokio::time::error::Elapsed),
+//     #[error("can't read stream '{0}'")]
+//     ReadStream(String),
+// }
