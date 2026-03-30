c343f21c15f3f6377fbedd173f240a77393fcfea
m3id <m3id.eprac@gmail.com>
2026-03-30 10:12:05 +0900

    refactor(request)!: simplify validate_access_token and add test helpers
    validate_access_token now uses the global client.
    to change validate_url or client in tests, enable the test feature and
    use ValidateRequest directly.

--- src/request/validate_request.rs
+++ src/request/validate_request.rs
@@ -1,15 +1,20 @@
+use std::str::FromStr;
+
 use asknothingx2_util::api::{AuthScheme, IntoRequestBuilder, Method};
 use reqwest::{Client, RequestBuilder, header::AUTHORIZATION};
 
 use crate::{AccessToken, Error, ValidateUrl, tokens::TokenInfo};
 
 /// <https://dev.twitch.tv/docs/authentication/validate-tokens/>
-pub async fn validate_access_token(
-    access_token: &AccessToken,
-    client: &Client,
-    validate_url: &ValidateUrl,
-) -> Result<TokenInfo, Error> {
-    crate::oauth::json(client, ValidateRequest::new(access_token, validate_url)).await
+pub async fn validate_access_token(access_token: &AccessToken) -> Result<TokenInfo, Error> {
+    crate::oauth::json(
+        crate::client::get(),
+        ValidateRequest::new(
+            access_token,
+            &ValidateUrl::from_str(crate::oauth::VALIDATE_URL).unwrap(),
+        ),
+    )
+    .await
 }
 
 /// <https://dev.twitch.tv/docs/authentication/validate-tokens/>
@@ -25,6 +30,19 @@ impl<'a> ValidateRequest<'a> {
             access_token,
             validate_url,
         }
+    }
+}
+
+#[allow(dead_code)]
+#[cfg(feature = "test")]
+impl<'a> ValidateRequest<'a> {
+    pub fn with_validate_url(mut self, url: &'a ValidateUrl) -> Self {
+        self.validate_url = url;
+        self
+    }
+
+    pub async fn send(self, client: &reqwest::Client) -> Result<TokenInfo, Error> {
+        crate::oauth::json(client, self).await
     }
 }
 
