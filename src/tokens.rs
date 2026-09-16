b3b9fcd6011151528372438b6b38f6f7abe2f883
Caymon / Kayo <24500457+giantcow@users.noreply.github.com>
2026-09-16 11:00:29 -0700

    fix(tokens): add created_at field in UserToken serialize (#2)
    * fix(UserToken): Serialize created_at
    
    Looks like this was missed when it was originally added in 089ebe7. Without this value, it's impossible to retain the created_at field when serializing/deserializing which is needed for checking if the token has expired
    
    * fix(tokens): update field count in UserToken serialize
    
    ---------
    
    Co-authored-by: m3id <99604520+m3idnotfree@users.noreply.github.com>

--- src/tokens.rs
+++ src/tokens.rs
@@ -30,12 +30,13 @@ impl Serialize for UserToken {
     {
         use serde::ser::SerializeStruct;
 
-        let mut state = serializer.serialize_struct("Token", 5)?;
+        let mut state = serializer.serialize_struct("Token", 6)?;
         state.serialize_field("access_token", &self.access_token)?;
         state.serialize_field("expires_in", &self.expires_in)?;
         state.serialize_field("token_type", &self.token_type)?;
         state.serialize_field("refresh_token", &self.refresh_token)?;
         state.serialize_field("scope", &self.scope)?;
+        state.serialize_field("created_at", &self.created_at)?;
         state.end()
     }
 }
